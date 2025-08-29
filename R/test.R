355ff74fa51709c02a8f1b2d2121dddcc3d7d0ad
Andrea Kipingu <andreakipingu@gmail.com>
2025-08-29 15:41:51 +0300

    This is the example file to test that the powergene package is working.

---
+++ R/test.R
@@ -0,0 +1,6 @@
+#instal package
+devtools::install_github('akipingu/powergene')
+#library
+library(powergene)
+# hello is the is the square function. any number you input, you will get its square.
+hello(3)
