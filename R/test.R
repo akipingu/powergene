1f10b12a80385146e546faa461e5414846e5db02
Andrea Kipingu <andreakipingu@gmail.com>
2025-08-30 19:58:27 +0300

    Updated the function with default values. Other files also updated.

--- R/test.R
+++
@@ -1,6 +0,0 @@
-#instal package
-devtools::install_github('akipingu/powergene')
-#library
-library(powergene)
-# hello is the is the square function. any number you input, you will get its square.
-hello(3)
